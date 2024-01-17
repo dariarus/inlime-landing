050e94767505ab5071c203c11307463a5f9c087a
dariarus <rusanova_dv@mail.ru>
2024-01-17 19:42:30 +0300

    feature: внедрить на страницу Яндекс-виджеты с отзывами и с картой.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -29,6 +29,7 @@ import {Service} from '../service/service';
 import {TImage} from '../../services/types';
 import {Promo} from '../promo/promo';
 import {Button} from '../button/button';
+import {SocialNetworks} from '../social-networks/social-networks';
 
 const App: FunctionComponent = () => {
   const [popupIsOpened, setPopupIsOpened] = useState<boolean>(false);
@@ -105,11 +106,41 @@ const App: FunctionComponent = () => {
                    description="Предложение актуально при записи на 20 число каждого месяца"/>
             <Promo promoImage={promoFirstVisit} heading="Знакомство с InLime! -300 руб на любые услуги"
                    description="Предложение актуально для клиентов, которые впервые записываются в студию"/>
-            <Promo promoImage={promoBirthday} heading="Подарок в день рождения! -25% на все услуги"
+            <Promo promoImage={promoBirthday} heading="Подарок в День рождения! -25% на все услуги"
                    description="Предложение актуально всю неделю до Вашего Дня рождения и неделю после"/>
           </div>
           <div className={appStyles['button-wrap']}>
-            <Button type="small" color="lime"/>
+            <Button color="lime"/>
+          </div>
+        </section>
+        <section className={appStyles.section}>
+          <h2 className={`${appStyles.section__text} ${appStyles.section__text_heading}`}>Отзывы о студии</h2>
+          <div className={appStyles.reviews}>
+            <iframe className={appStyles.reviews__iframe}
+                    src="https://yandex.ru/maps-reviews-widget/106556456544?comments">
+            </iframe>
+            <a href="https://yandex.ru/maps/org/inlime/106556456544/" target="_blank"
+               className={appStyles.reviews__link}>InLime
+              на карте Новороссийска — Яндекс Карты</a>
+          </div>
+        </section>
+        <section className={appStyles.section}>
+          <h2 className={`${appStyles.section__text} ${appStyles.section__text_heading}`}>Контакты</h2>
+          <div className={appStyles.contacts}>
+            <SocialNetworks/>
+            <p className={`${appStyles.text} ${appStyles.text_contacts}`}>+7 (964) 893-23-68</p>
+            <p className={`${appStyles.text} ${appStyles.text_contacts}`}>г. Новороссийск, 15 мкр-н, ул Южная, 9</p>
+            <div className={appStyles.contacts__map}>
+              <a href="https://yandex.ru/maps/org/inlime/106556456544/?utm_medium=mapframe&utm_source=maps"
+                 className={`${appStyles.contacts__link} ${appStyles.contacts__link_primary}`}>InLime</a>
+              <a
+              href="https://yandex.ru/maps/970/novorossiysk/category/beauty_salon/184105814/?utm_medium=mapframe&utm_source=maps"
+              className={`${appStyles.contacts__link} ${appStyles.contacts__link_secondary}`}></a>
+              <iframe
+                src="https://yandex.ru/map-widget/v1/?ll=37.783220%2C44.675963&mode=search&oid=106556456544&ol=biz&z=15.14"
+                className={appStyles.contacts__iframe}
+                allowFullScreen={true}></iframe>
+            </div>
           </div>
         </section>
         {
