51075ff5e1aa4048591d50cb03bc648e55449b6b
dariarus <rusanova_dv@mail.ru>
2024-02-10 18:46:13 +0000

    feature: добавить библиотеку vsharp для сжатия изображений при билде

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import vsharp from 'vite-plugin-vsharp';
 
 export default defineConfig({
   plugins: [
     react({
       tsDecorators: true
     }),
+    vsharp(),
   ],
   base: "/",
 })
